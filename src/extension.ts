df5aed9f9b8b619a7a0a101ec01656f96164cd32
Halis <67235745+nothing-githb@users.noreply.github.com>
2023-06-14 23:27:46 +0300

    commands implemented

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,200 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "swottgo" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "swottgo" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("swottgo.helloWorld", () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage("Hello World from SwotTgo!");
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('swottgo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SwotTgo!');
-	});
+  vscode.commands.registerCommand(
+    "extension.cleanBuild",
+    async (uri: vscode.Uri) => {
+      processCommand(uri, "cleanBuild", false);
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  vscode.commands.registerCommand(
+    "extension.changeBsp",
+    async (uri: vscode.Uri) => {
+      processCommand(uri, "changeBsp", false);
+    }
+  );
+
+  vscode.commands.registerCommand(
+    "extension.downloadToTarget",
+    async (uri: vscode.Uri) => {
+      processCommand(uri, "downloadToTarget", true);
+    }
+  );
+
+  vscode.commands.registerCommand(
+    "extension.changeBspAndCleanBuild",
+    async (uri: vscode.Uri) => {
+      processCommand(uri, "changeBspAndCleanBuild", false);
+    }
+  );
+
+  vscode.commands.registerCommand(
+    "extension.cleanBuildAndDownloadToTarget",
+    async (uri: vscode.Uri) => {
+      processCommand(uri, "cleanBuildAndDownloadToTarget", false);
+    }
+  );
+
+  vscode.commands.registerCommand(
+    "extension.changeBspAndCleanBuildAndDownloadToTarget",
+    async (uri: vscode.Uri) => {
+      processCommand(uri, "changeBspAndCleanBuildAndDownloadToTarget", false);
+    }
+  );
+
+  vscode.commands.registerCommand(
+    "extension.objDump",
+    async (uri: vscode.Uri) => {
+      processCommand(uri, "objDump", true);
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function extractStringsFromCurlyBraces(input: string): string[] {
+  const regex = /{([^}]+)}/g;
+  const matches = input.match(regex);
+
+  if (matches) {
+    return matches.map((match) => {
+      // Remove the curly braces from the matched string
+      return match.slice(1, -1);
+    });
+  }
+
+  return [];
+}
+
+async function processCommand(
+  uri: vscode.Uri,
+  command: string,
+  isFullPath: boolean
+) {
+  let itemAbsString: string;
+  let itemRelString: string;
+
+  if (uri) {
+    try {
+      // Get the string of the clicked item
+      itemAbsString = uri.fsPath;
+      itemRelString = path.basename(uri.fsPath);
+
+    } catch (error) {
+      vscode.window.showErrorMessage(`Failed to get item information ${error}`);
+    }
+  }
+
+  // Get the configuration for the current workspace
+  const configuration = vscode.workspace.getConfiguration("swotide");
+
+  // Access the extension-specific variable from the user settings
+  const jsonFilePath = configuration.get<string>("jsonFile");
+  const cleanBuildCommand = configuration.get<string>(command);
+
+  // Use the retrieved variable
+  console.log(jsonFilePath);
+  console.log(cleanBuildCommand);
+
+  if (!jsonFilePath) {
+    vscode.window.showErrorMessage(
+      "SwotIDE: Json file is not found!(swotide.jsonFile)"
+    );
+    return;
+  }
+
+  if (!cleanBuildCommand) {
+    vscode.window.showErrorMessage(
+      "SwotIDE: Clean build command is not found!(swotide.cleanBuild)"
+    );
+    return;
+  }
+
+  let newCleanBuildCommand: string = cleanBuildCommand;
+
+  fs.readFile(jsonFilePath.toString(), "utf-8", (err, data) => {
+    if (err) {
+      vscode.window.showErrorMessage(
+        `SwotIDE: Failed to read JSON file: ${err}`
+      );
+      return;
+    }
+
+    // Parse the JSON data
+    try {
+      const jsonData = JSON.parse(data);
+
+      const extractedStrings = extractStringsFromCurlyBraces(cleanBuildCommand);
+      console.log(extractedStrings);
+
+      for (const elem of extractedStrings) {
+        if (elem === "clicked_abs_path") {
+          newCleanBuildCommand = newCleanBuildCommand.replace(
+            "{" + elem + "}",
+            itemAbsString
+          );
+        } 
+        else if (elem === "clicked_rel_path") {
+          newCleanBuildCommand = newCleanBuildCommand.replace(
+            "{" + elem + "}",
+            itemRelString
+          );
+        }
+        else {
+          const propertyPath = elem;
+          const pathParts = propertyPath.split(".");
+
+          let result = jsonData;
+          for (const part of pathParts) {
+            if (result.hasOwnProperty(part)) {
+              result = result[part];
+            } else {
+              result = undefined;
+              break;
+            }
+          }
+          newCleanBuildCommand = newCleanBuildCommand.replace(
+            "{" + elem + "}",
+            result
+          );
+        }
+        console.log(elem);
+      }
+      console.log(newCleanBuildCommand);
+
+      // Create a new terminal
+      const terminal = vscode.window.createTerminal("SwotIDE Terminal");
+
+      // Send the command to the terminal
+      terminal.sendText(newCleanBuildCommand);
+
+      // Show the terminal
+      terminal.show();
+    } catch (err) {
+      vscode.window.showErrorMessage(`Failed to parse JSON file: ${err}`);
+    }
+  });
+}
